ab40d4976a2bf2bf15df17a104858d02c2363451
Mikhail170 <ga_pe@mail.ru>
2024-12-30 10:15:15 +0200

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,20 @@
+# 1st program
+print((9**0.5) * 5)
+
+#2nd program
+print((9.99 > 9.98 and 1000 != 1000.1))
+
+#3rd program
+a = 2 * 2 +2
+print(a)
+b = (2 + 2) * 2
+print(b)
+print(a == b)
+
+#4th program
+str_ = '123.456'
+float_number = float(str_)
+shifted_number = float_number * 10
+int_number = int(shifted_number)
+first_digit_after_decimal = int_number % 10
+print(first_digit_after_decimal)
